9f1322f9b2578c725cba7b24eb9b5660e8a53695
AaronArdz <aaron_acostarz@outlook.es>
2020-11-21 17:51:01 -0600

    added echarts library

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuelidate from 'vuelidate'
+import './assets/dist/dist/css/adminlte.css';
+import './assets/dist/dist/css/adminlte.min.css';
+
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
